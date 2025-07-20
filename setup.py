d73e9cef801aeddcc782c9ee254df9f940a58697
Aarav Maloo <aaravmaloo06@gmail.com>
2025-07-20 19:43:24 +0530

    increased version for pypi upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="bugmark",
-    version="0.1.0",
+    version="0.1.1",
     author="Aarav Maloo",
     author_email="your@email.com",
     description="A command-line bug marker utility for programmers.",
