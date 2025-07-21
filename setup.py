462ec0f5978d48de41203f1b60e0a8aeb5474bbe
Aarav Maloo <aaravmaloo06@gmail.com>
2025-07-21 23:08:03 +0530

    updated setup for python version changes

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     entry_points={
         "console_scripts": [
             "bugmark=bugmark.bugmark:main",
